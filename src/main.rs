e63143a845c56fbbb27e9a034b1c6645c7bcbf72
Antoine d'Aligny <antoinedaligny@outlook.fr>
2020-09-14 10:37:03 +0200

    Version 0.1.1 : Add port config Improve output format with more info

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use async_std::task;
-use colored::Colorize;
+use colored::{ColoredString, Colorize};
 use std::{
     future::Future,
     path::{Path, PathBuf},
@@ -8,6 +8,7 @@ use std::{
         Arc,
     },
 };
+use std::{io::Write, time};
 use tide::{http::Mime, Body, Request, Response, StatusCode};
 
 #[derive(argh::FromArgs)]
@@ -17,14 +18,17 @@ struct Args {
     #[argh(switch, short = 'o')]
     pub open: bool,
     /// the path to the source directory
-    #[argh(option, default = "String::from(\".\")")]
+    #[argh(option, short = 'd', default = "String::from(\".\")")]
     pub dir: String,
-    // TODO: add port config
+    /// the port for the server
+    #[argh(option, short = 'p', default = "8080")]
+    pub port: u16,
 }
 
 fn main() {
     let args: Args = argh::from_env();
     let open = args.open;
+    let port = args.port;
     let dir = PathBuf::from(args.dir);
 
     let version_counter = Arc::new(AtomicU16::new(0));
@@ -34,6 +38,7 @@ fn main() {
         hotwatch
             .watch(dir.clone(), move |_| {
                 counter.fetch_add(1, Ordering::Relaxed);
+                println!(" - {}", "Reload".white().bold());
             })
             .expect("Failed to watch source directory");
     }
@@ -53,35 +58,65 @@ fn main() {
             let version = version_counter.load(Ordering::Relaxed);
             async move { Ok(format!("{}", version)) }
         });
+        let address = format!("127.0.0.1:{}", port);
+        let html_address = format!("http://{}", address);
         if open {
-            open::that("http://127.0.0.1:8080").unwrap();
+            open::that(&html_address).unwrap();
         }
-        println!("Online at http://127.0.0.1:8080");
-        app.listen("127.0.0.1:8080").await.unwrap();
+        println!(" - {}: {}", "Open".white().bold(), html_address);
+        app.listen(address).await.unwrap();
     })
 }
 
+fn format_result_code(status: StatusCode) -> ColoredString {
+    if status.is_client_error() || status.is_server_error() {
+        format!("{}", status).red()
+    } else if status.is_redirection() || status.is_informational() {
+        format!("{}", status).yellow()
+    } else {
+        format!("{}", status).green()
+    }
+}
+
 fn serve_file(dir: &Path, req: Request<()>) -> impl Future<Output = tide::Result<Response>> {
+    let start = time::Instant::now();
     let mut res = Response::new(StatusCode::Ok);
     let arg: String = req.param("path").unwrap_or("".into());
     let path = if arg == "" { "index.html" } else { &arg };
-    let mut content = match std::fs::read_to_string(dir.join(path)) {
-        Ok(content) => {
-            println!("{} /{}", "OK ".green(), arg);
-            content
-        }
-        Err(err) => {
-            println!("{} /{}", "ERR".red(), arg);
-            return async_std::future::ready(Err(err.into()));
-        }
+    let end = time::Instant::now();
+    let time = end.duration_since(start).as_micros() as f64 / 1000.0;
+    let dash = "-".bright_black().bold();
+    let instant = chrono::Local::now().format("%T").to_string().purple();
+
+    let (content, status) = match std::fs::read_to_string(dir.join(path)) {
+        Ok(content) => (Some(content), StatusCode::Ok),
+        Err(_err) => (None, StatusCode::NotFound),
     };
-    if path.ends_with(".html") {
-        content.push_str(include_str!("./hot.html"));
+    println!(
+        "[{}] {} {} {}ms {} /{}",
+        instant,
+        format_result_code(status),
+        dash,
+        time,
+        dash,
+        arg
+    );
+    res.set_status(status);
+
+    if let Some(content) = content {
+        let mut content = content.clone();
+        if path.ends_with(".html") {
+            content.push_str(include_str!("./hot.html"));
+        }
+        let mut body = Body::from_string(content);
+
+        if let Some(mime) = Mime::from_extension(String::from(path).split(".").last().unwrap_or(""))
+        {
+            body.set_mime(mime);
+        }
+
+        res.set_body(body);
     }
-    let mut body = Body::from_string(content);
-    if let Some(mime) = Mime::from_extension(String::from(path).split(".").last().unwrap_or("")) {
-        body.set_mime(mime);
-    }
-    res.set_body(body);
+
     async_std::future::ready(Ok(res))
 }
